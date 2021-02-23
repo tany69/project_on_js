c949b39c0b782c9b601c626ee2999289a4643bd6
Tatyana <tany69@yandex.ru>
2021-02-23 16:51:05 +0800

    less 3 begin

--- js/main.js
+++ js/main.js
@@ -1,58 +1,82 @@
+const api=`https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses`;
+
+
+let getRequest= (url,cb)=> {
+    let xhr= new XMLHttpRequest();
+    xhr.open('get',url,true);
+    xhr.onreadystatechange = ()=>{
+        if(xhr.readyState !== 4){
+            return;
+        }
+        if (xhr.status !==200){
+            console.log('some err');
+            return;
+        }
+        cb(xhr.responseText);
+    }
+};
+
+
+
 class Products {
-    data=[];
     products=[];
     container= null;
     container_sum = null;
     summa=0;
     constructor(selector){
         this.container = document.querySelector(selector);
-        this._fetchData();
-        this._render();
-        this._fetch_summa('.itog');
+        this._fetchData()
+            .then(()=>this._render());
+            //this.products._fetch_summa('.itog');
+
     }
 
     _fetchData(){
-        this.data= [ { title: 'Notebook',id:1,price:2000},
-            { title: 'Keyboard',id:2,price:200},
-            { title: 'Mouse',id:3,price:100},
-            { title: 'Gamepad',id:4, price:500},
-            {id:5,price:1000},
-            { title: 'Web camera', id:6, price:500}
-        ];
+        return fetch(`${api}/catalogData.json`)
+        .then(result => result.json())
+        .then(data => {
+            for(let product of data){
+                this.products.push(new ProductItem(product));
+            }
+        })
+
     }
 
     _render(){
-        for(let data of this.data){
-            const product = new ProductItem(data);
-            this.products.push(product);
-            // подсчет суммы всех товаров
-            this.summa +=product.price;
+        for(let product of this.products){
+            if(product.rendered){
+                continue;
+            }
             this.container.insertAdjacentHTML('beforeend',product.render());
 
         }
     }
     _fetch_summa(selector){
-        this.container_sum = document.querySelector(selector); // определяем новый контейнер где вставить строку
-        console.log(this.container_sum); // в консоль выводиться
-        console.log(this._render_all_summa()); // и это выводится
-        this.container_sum.insertAdjencentHTML('afterbegin', this._render_all_summa()); // здесь выдает ошибку. я не понимаю почему!!!!
+        this.container_sum = document.querySelector(selector);
+        for(let i=0;i<this.products.length;i++) {
+            this.summa += this.products[i].price;
+        }
+        this.container_sum.insertAdjacentHTML('afterbegin', this._render_all_summa());
     }
+
     _render_all_summa(){
         return `<p>Стоимость товаров в каталоге : ${this.summa}</p>`;
     }
 
 }
- console.log();
+console.log();
 class ProductItem{
     title='';
     price= 0;
     id  =0;
     img='';
+    rendered =false;
     constructor(product,img = 'pics/img.png'){
-        ({title:this.title, id:this.id,price:this.price}=product);
+        ({product_name:this.title, id_product:this.id,price:this.price}=product);
         this.img= img;
     }
     render(){
+        this.rendered= true;
         return `<div class="product-item"> 
                 <h3>${this.title}</h3>
                 <img src="${this.img}" class="product-img">
@@ -73,30 +97,28 @@ let list =new Products('.products');
 // class Cart{
 //     listItem = [];  - массив  содержащий параметры товара (title, id, price,counytItem,img) -
 //     container = null;
-//     _cartItemClick(){}   - метод  получения свойств элемента каталога, получение массива  с данными выбранного товара
 //     _calcCountItems(){}  - метов подсчета количества всех товаров в корзине
 //     _calcSumItems(){}    - метов подсчета суммы всех товаров в корзине
-//     render(){}           - метов отображения корзины с элементами и кнопкой "Оформить заказ"
+//     сartItemAdd(){}    - метод для вывода выбранного товара. Выводиться будет последним
+//     cartItemDel(){}   - метод удаления товара из корзины.
+//     HideShowCart(){} - метод отображения и скрытия корзины
+//     cartItemCounter(ItemId,flag){}- Метод счетчик товара в корзине, работает как калькулятор при клике на + или - для увеличения количества товара в корзине и считает сумму  товара
 //
 // }
 
 //создание класса для элемента корзины
 // class CartItem{
-    // title=''; -наименование элемента корзины
-    // price= 0; - цена элемента корзины
-    // id  =0; - уникальный номер элемента корзины
-    // img=''; - миниатюра изображение элемента корзины
-    // countItem = 0; - кол-во элементов в корзине
-    // constructor(product,count = 1,img = 'pics/img_mini.png'){
-    //     ({title:this.title, id:this.id,price:this.price}=product);
-    //     this.img= img;
-    //     this.countItem =count;
-    // }
-
-   // cartItemCounter(ItemId,flag){}- Метод счетчик товара в корзине, работает как калькулятор при клике на + или - для увеличения количества товара в корзине.
-   // calcSumma(){} - метод подсчета суммы при увеличении/ уменьшении счетчика товара
-   // cartItemDel(){}   - метод удаления товара из корзины.
-   // render(){}        - метод  формирования блока кода  html  для вывода товара в корзине.
+// title=''; -наименование элемента корзины
+// price= 0; - цена элемента корзины
+// id  =0; - уникальный номер элемента корзины
+// img=''; - миниатюра изображение элемента корзины
+// countItem = 0; - кол-во элементов в корзине
+// constructor(product,count = 1,img = 'pics/img_mini.png'){
+//     ({title:this.title, id:this.id,price:this.price}=product);
+//     this.img= img;
+//     this.countItem =count;
 // }
 
 
+// render(){}        - метод  формирования блока кода  html  для вывода товара в корзине.
+// }
